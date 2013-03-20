6bf918aeec7559e03eda827f31ad8923524eaa70
root <root@localhost.localdomain>
2013-03-20 12:20:12 +0000

    play 2.1 version of app

---
+++ 7digiscala/app/models/Track.scala
@@ -0,0 +1,8 @@
+package models
+
+class Track(ctitle: String, cartist: String, cimage: String)
+{
+     var title: String = ctitle
+     var artist: String = cartist
+     var image: String = cimage
+}
