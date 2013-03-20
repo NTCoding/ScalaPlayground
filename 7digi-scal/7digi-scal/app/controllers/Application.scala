a1ce5a5af430fff96c825334c1a934015d237616
Nick <nick@ntcoding.co.uk>
2013-03-20 08:42:13 +0000

    preparing to use 7digital api to get results for home page

--- 7digi-scal/7digi-scal/app/controllers/Application.scala
+++ 7digi-scal/7digi-scal/app/controllers/Application.scala
@@ -2,15 +2,30 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.libs._
 import models._
 
 
-object Application extends Controller {
+object Application extends Controller
+{
   
-  def index = Action {
-    // create some dummy DTOs and send them to the view
-    val chart = List( new Track("amazing", "your mom", "http://www.google.com"), new Track("brilliant", "your dad", "http://www.google.com"))
-    Ok(views.html.index(chart))
-  }
-  
-}
+    def index = Action
+    {
+      Ok(views.html.index(get7digitalTrackChart()))
+    }
+
+
+    private def get7digitalTrackChart(): List[Track] =
+    {
+      val chartUrl = "http://api.7digital.com/1.2/track/chart?period=week&toDate=20120301&oauth_consumer_key=YOUR_KEY_HERE&country=GB&pagesize=2"
+      val response = WS.url(chartUrl).get.get().getBody
+      val tracks =  parseTracks(response)
+      tracks
+    }
+
+    private def parseTracks(body: String) : List[Track] =
+    {
+        List( new Track("wicked", "big", "blazin"))
+    }
+
+ }
