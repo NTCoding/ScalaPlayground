4fba41787a699f2b95ef4c4d4b715db5bfbd45e8
root <root@localhost.localdomain>
2013-03-21 12:42:09 +0000

    deleted old version of 7digi site

--- 7digi-scal/7digi-scal/app/models/Track.scala
+++
@@ -1,8 +0,0 @@
-package models
-
-class Track(ctitle: String, cartist: String, cimage: String)
-{
-     var title: String = ctitle
-     var artist: String = cartist
-     var image: String = cimage
-}
