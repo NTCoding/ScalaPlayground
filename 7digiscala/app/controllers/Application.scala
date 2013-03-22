a6dc525b708fb7c1975230a4f51aa155e621793f
nick <nick@ntcoding.co.uk>
2013-03-22 12:00:07 +0000

    json responses added for track chart

--- 7digiscala/app/controllers/Application.scala
+++ 7digiscala/app/controllers/Application.scala
@@ -1,38 +1,45 @@
 package controllers
 
 
-import play.api.mvc._
+
 import models._
-import play.libs._
-import org.w3c._
+import play.libs.WS
 import org.w3c.dom.Document
 import org.w3c.dom.Node
 import org.w3c.dom.Element
 import services._
+import play.api.libs.json.Json
+import play.api.mvc.Controller
+import play.api.mvc.Action
+
 
 
 object Application extends Controller
 {
   
-    def index = Action
-    {
-      Ok(views.html.index(get7digitalTrackChart()))
+    def index = Action  { implicit request =>
+    		val chart = get7digitalTrackChart()
+    		render{
+    			case Accepts.Html() => Ok(views.html.index(chart))
+    			case Accepts.Json() => Ok(Json.obj("tracks" -> Json.arr(
+    					chart.map(t => Json.obj("title" -> t.title, "artist" -> t.artist, "artistUrl" -> t.artistUrl, "image" -> t.image))
+    			)))
+    		}
     }
-
 
     private def get7digitalTrackChart(): Seq[Track] =
     {
-      val response = WS.url("http://api.7digital.com/1.2/track/chart")
-                       .setQueryParameter("period", "week")
-                       .setQueryParameter("oauth_consumer_key", "test-api")
-                       .setQueryParameter("toDate", "20120301")
-                       .setQueryParameter("imageSize", "100")
-                       .get
-                       .get()
-                       .getBody()
-
-      val tracks =  TrackParser.parseChartItem(response)
-      tracks
+    		val response = WS.url("http://api.7digital.com/1.2/track/chart")
+											.setQueryParameter("period", "week")
+											.setQueryParameter("oauth_consumer_key", "test-api")
+											.setQueryParameter("toDate", "20120301")
+											.setQueryParameter("imageSize", "100")
+											.get
+											.get()
+											.getBody()
+		
+			val tracks =  TrackParser.parseChartItem(response)
+			tracks
     }
 
  }
--- 7digiscala/app/services/TrackParser.scala
+++ 7digiscala/app/services/TrackParser.scala
@@ -11,25 +11,25 @@ object TrackParser
 {
     def parseChartItem(body: String) : Seq[Track] =
     {
-	val trackTags = XML.loadString(body) \\ "chartItem"
-        val tracks = trackTags.map(tag => buildTrack(tag))
-        tracks
+		    val trackTags = XML.loadString(body) \\ "chartItem"
+	        val tracks = trackTags.map(tag => buildTrack(tag))
+	        tracks
     }
 
     def parseSearchResult(body: String) : Seq[Track] =
     {
-      val trackTags = XML.loadString(body) \\ "searchResult"
-      val tracks = trackTags.map(tag => buildTrack(tag))
-      tracks
+	      val trackTags = XML.loadString(body) \\ "searchResult"
+	      val tracks = trackTags.map(tag => buildTrack(tag))
+	      tracks
     }
 	
     private def buildTrack(tag: Node) : Track =
     {
-    	val title = tag \ "title" text
-       	val artist = tag \ "track" \ "artist" \ "name" text
-       	val image = tag \ "track" \ "release" \ "image" text
-        val artistUrl = tag \ "artist" \ "url" text
-       					    	
-        new Track(title, artist, image, artistUrl)
+	    	val title = tag \ "track" \ "title" text
+	       	val artist = tag \ "track" \ "artist" \ "name" text
+	       	val image = tag \ "track" \ "release" \ "image" text
+	        val artistUrl = tag \ "artist" \ "url" text
+	       					    	
+	        new Track(title, artist, image, artistUrl)
     }
 }
