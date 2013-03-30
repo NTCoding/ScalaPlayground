f57e1b35818cd81ec6e631ff8f07a421823ed513
Nick <nick@ntcoding.co.uk>
2013-03-30 17:55:44 +0000

    added chapter 2 and an sbt example

--- scala_in_action/chapter_2/pattern_match.scala
+++ scala_in_action/chapter_2/pattern_match.scala
@@ -4,3 +4,11 @@ def sayMyName (name: String) = name match {
 	case "Egg" => println("Love you Egg")
 	case _ => println("Gday fella")
 }
+
+def giveMeAKiss (name: String) = { 
+	val nicePeople = List("Mum", "Nan", "Auntie")
+	name match {
+		case nicePerson if nicePeople contains nicePerson => println("Kissy kissy")
+		case _ => println("get lost")
+	}
+}
---
+++ scala_in_action/chapter_2/rest_client/build.sbt
@@ -0,0 +1,5 @@
+name := "RestClient"
+
+scalaVersion := "2.10.1"
+
+libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.1.2"
