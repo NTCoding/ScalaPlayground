0ce54131dd25b94b76d776d8183eb4cc80842e5b
Nick <nick@ntcoding.co.uk>
2013-04-01 12:22:32 +0100

    refactored args parser to be recursive

--- thermal_analyser/src/main/scala/SimpleArgsParser.scala
+++ thermal_analyser/src/main/scala/SimpleArgsParser.scala
@@ -6,23 +6,13 @@ class SimpleArgsParser extends ArgsParser {
 		val chars = sizeAndGrid.split(" ")
 		val numberOfResults = chars(0).toInt
 		val gridSize = chars(1).toInt
-		(numberOfResults, new Grid(buildRows(gridSize, chars)))
+		(numberOfResults, new Grid(split(gridSize, List[List[Int]](), chars.splitAt(2)._2.map(_.toInt).toList)))
 	}
 
-	def buildRows(gridSize: Int, numbers: Array[String]) = {
-		var rows = List[List[Int]]()
-		var list = List[Int]()
-		
-		for (i <- 2 to numbers.size - 1) {
-			if (list.size == gridSize) {
-				rows = rows :+ list
-				list = List(numbers(i).toInt)
-			} 
-			else {
-				list = list :+ numbers(i).toInt
-			}
+	def split(gridSize: Int, rows: List[List[Int]], remaining: List[Int]) : List[List[Int]] = {
+		remaining.size match {
+			case 0 => rows 
+			case _ => split(gridSize, rows :+ remaining.splitAt(gridSize)._1, remaining.splitAt(gridSize)._2)
 		}
-		
-		rows :+ list
 	}
 }
