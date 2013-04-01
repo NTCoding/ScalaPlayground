4745fcdfd8106639275e34c4a187696f02029b17
Nick <nick@ntcoding.co.uk>
2013-04-01 07:58:08 +0100

    refactored analyser

--- thermal_analyser/src/main/scala/Analyser.scala
+++ thermal_analyser/src/main/scala/Analyser.scala
@@ -2,11 +2,10 @@ package thermal_analyser
 
 class Analyser(private val parser: ArgsParser, private val calc: ScoreCalculator, private val formatter: ResultFormatter) {
 
-	def analyse(sizeAndGrid: String) = parser.parse(sizeAndGrid) match {
-
-		case (size, grid) => formatter.format(calc.calculate(size, grid))
-
-		case _ => "Error"	
+	def analyse(sizeAndGrid: String) = {
+		val (size, grid) = parser.parse(sizeAndGrid)
+		val scores = calc.calculate(size, grid)
+		formatter.format(scores)
 	}
 	
 }
