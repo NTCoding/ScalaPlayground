a6b4465edfda119aee6d3fac57a2d99db7304069
Nick <nick@ntcoding.co.uk>
2013-04-10 10:56:13 +0100

    esb now delivers message to the correct queue

--- akka/akka_esb/src/main/scala/Host/scalaEsbHost.scala
+++ akka/akka_esb/src/main/scala/Host/scalaEsbHost.scala
@@ -1,12 +1,34 @@
 package Host
 
-class scalaEsbHost(val configuration: Unit) {
+import com.rabbitmq.client._
+
+class scalaEsbHost(val configuration: List[List[Any]]) {
 	
-	def publish(event: Unit) {
-		// get the name of the event
+	def publish(event: Any) {
+		event match {
+			case (eventName: String, data: (Any)) => publishToConsumers(eventName, data)
+			case _ => println("Not a valid event")
+		}
 	}
 	
-	def start {
-		
+	def publishToConsumers(eventName: String, data: (Any)) {
+		val consumersOfEvent = configuration.filter(l => l.head.toString == eventName)(0).tail
+		for((host, service) <- consumersOfEvent){
+			sendMessageToQueue(service.toString, (eventName, data))
+		}
 	}
+	
+	def sendMessageToQueue(service: String, message: (String,(Any))) {
+		val factory = new ConnectionFactory()
+		val c = factory.newConnection()
+		val ch = c.createChannel()
+		ch.exchangeDeclare(service, "direct", true)
+		ch.queueDeclare(service, true, false, false, null)
+		ch.queueBind(service, service, service)
+		ch.basicPublish(service, service, null, message.toString.getBytes)
+		ch.close
+		c.close
+	}
+	
+	def start {	}
 }
