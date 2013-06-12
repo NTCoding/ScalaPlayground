c56ae8c4630be55a4899984d0975ad3786212314
Nick <nick@ntcoding.co.uk>
2013-06-12 16:49:48 +0100

    added function composition example

---
+++ functional_programming_in_scala/chapter_2/src/test/scala/FunctionCompositionTest.scala
@@ -0,0 +1,14 @@
+import org.scalatest.FreeSpec
+import org.scalatest.matchers.MustMatchers
+
+class FunctionCompositionTest extends FreeSpec with MustMatchers {
+
+    "compose allows a code builder to build codes from ints and chars" in {
+        val resultFunc = compose[List[Int], List[Double], String](x =>  x mkString " ", y => y map (_ + 1.5))
+        resultFunc(List(1, 2, 3)) must equal("2.5 3.5 4.5")
+    }
+
+    def compose[A, B, C](f: B => C, g: A => B): A => C = {
+        (x: A) => f(g(x))
+    }
+}
