dd90801dec5ed61f865101043b125190e9a7a58c
Nick <nick@ntcoding.co.uk>
2013-06-12 16:51:46 +0100

    added function composition test that uses compose syntactic sugar

--- functional_programming_in_scala/chapter_2/src/test/scala/FunctionCompositionTest.scala
+++ functional_programming_in_scala/chapter_2/src/test/scala/FunctionCompositionTest.scala
@@ -11,4 +11,13 @@ class FunctionCompositionTest extends FreeSpec with MustMatchers {
     def compose[A, B, C](f: B => C, g: A => B): A => C = {
         (x: A) => f(g(x))
     }
+
+    "compose syntactic sugar also works the same as above" in {
+        val resultFunc = composeWith[List[Int], List[Double], String](x =>  x mkString " ", y => y map (_ + 1.5))
+        resultFunc(List(1, 2, 3)) must equal("2.5 3.5 4.5")
+    }
+
+    def composeWith[A, B, C](f: B => C, g: A => B): A => C = {
+        f compose g
+    }
 }
