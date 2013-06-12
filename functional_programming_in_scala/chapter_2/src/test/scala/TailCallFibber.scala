5bbc35a477c8955af354bb170d356b4ad123ecbd
Nick <nick@ntcoding.co.uk>
2013-06-12 15:17:14 +0100

    added tail recursive fibonacci example

---
+++ functional_programming_in_scala/chapter_2/src/test/scala/TailCallFibber.scala
@@ -0,0 +1,35 @@
+import annotation.tailrec
+import org.scalatest.FreeSpec
+import org.scalatest.matchers.MustMatchers
+
+class TailCallFibber extends FreeSpec with MustMatchers {
+
+    "the 0th fib is 0" in {
+        fib(0) must equal(0)
+    }
+
+
+    "the 5th fibonacci number is 5" in {
+        fib(5) must equal(5)
+    }
+
+    "the 14th fib is 233" in {
+        fib(14) must equal(377)
+    }
+
+
+    def fib(n: Int) = {
+
+        @tailrec
+        def fib(n: Int, iteration: Int, numbers: Array[Int]) : Int = {
+            if (n <= 1) return n
+            iteration match {
+                case end if end >= n => numbers.last + numbers.reverse.tail.head
+                case otherwise => fib(n, iteration + 1, numbers :+ (numbers.last + numbers.reverse.tail.head))
+            }
+        }
+
+       fib(n, 2, Array(0, 1))
+    }
+
+}
