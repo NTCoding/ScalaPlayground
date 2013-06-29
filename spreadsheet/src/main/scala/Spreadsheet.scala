b5cd563027f32b31b0bc2c1902cd2a47d41a48db
nick <nick@ntcoding.co.uk>
2013-06-29 08:58:07 +0100

    added minimum in range

--- spreadsheet/src/main/scala/Spreadsheet.scala
+++ spreadsheet/src/main/scala/Spreadsheet.scala
@@ -44,6 +44,18 @@ class Spreadsheet {
         for (c <- startCol to endCol; r <- startRow to endRow) yield(validColumns(c), r, getCellValue(validColumns(c), r))
     }
 
+    /*
+        could put the maths functions in a new class - but that means 2 classes and a 3rd class to coordinate
+        between them - unnecessary complexity imo
+     */
+    def sumRange(start: (String, Int), end: (String, Int)) = getRange(start, end).map(_._3.toDouble).sum
+
+    def countRange(start: (String, Int), end: (String, Int)) = getRange(start, end).filter(_._3 != "").length
+
+    def maxInRange(start: (String, Int), end: (String, Int)) = getRange(start, end).map(_._3.toDouble).max
+
+    def minInRange(start: (String, Int), end: (String, Int)) = getRange(start, end).map(_._3.toDouble).min
+
     private def cellMustBeWithinGridRange(column: String, row: Int) {
         val isInRange = validColumns.contains(column) && row >= 1 && row <= 10
         if(!isInRange) throw new GridCellOutOfRange
--- spreadsheet/src/test/scala/Spreadsheet_spec.scala
+++ spreadsheet/src/test/scala/Spreadsheet_spec.scala
@@ -112,7 +112,7 @@ class Spreadsheet_spec extends FreeSpec with TableDrivenPropertyChecks with Must
         range must equal(Vector(("C", 2, "4.0"), ("C", 3, "34.3"), ("C", 4, "99.0"), ("C", 5, "28.1")))
     }
 
-    "Multi-line ranges can be selected" in {
+    "When a range of cells have been populated with values" - {
         val sp = new Spreadsheet
         sp.setCellValue("C", 2, 4)
         sp.setCellValue("C", 3, 34.3)
@@ -123,14 +123,29 @@ class Spreadsheet_spec extends FreeSpec with TableDrivenPropertyChecks with Must
         sp.setCellValue("D", 4, 3.3)
         sp.setCellValue("D", 5, 4.4)
 
-        val range = sp.getRange(("C", 2), ("D", 5))
+        "Multi-line ranges can be selected" in {
+            val range = sp.getRange(("C", 2), ("D", 5))
+            range must equal(Vector(("C", 2, "4.0"), ("C", 3, "34.3"), ("C", 4, "99.0"), ("C", 5, "28.1"), ("D", 2, "1.1"), ("D", 3, "2.2"), ("D", 4, "3.3"), ("D", 5, "4.4")))
+        }
 
-        range must equal(Vector(("C", 2, "4.0"), ("C", 3, "34.3"), ("C", 4, "99.0"), ("C", 5, "28.1"), ("D", 2, "1.1"), ("D", 3, "2.2"), ("D", 4, "3.3"), ("D", 5, "4.4")))
+        "Out of bounds ranges cause an error" in {
+            intercept[GridCellOutOfRange](sp.getRange(("A", 0), ("A", 1)))
+        }
+
+        "Ranges can be summed" in {
+            sp.sumRange(("C", 2), ("D", 5)) must equal(176.4)
+        }
+
+        "Number of non-empty cells in a range is returned by count" in {
+            sp.countRange(("C", 1), ("D", 6)) must equal(8)
+        }
+
+        "Maximum value in range is returned by max" in {
+            sp.maxInRange(("C", 2), ("D", 5)) must equal(99)
+        }
+
+        "Minimum value in range is returned by min" in {
+            sp.minInRange(("C", 2), ("D", 5)) must equal(1.1)
+        }
     }
-
-    "Out of bounds ranges cause an error" in {
-        val sp = new Spreadsheet
-        intercept[GridCellOutOfRange](sp.getRange(("A", 0), ("A", 1)))
-    }
-
 }
