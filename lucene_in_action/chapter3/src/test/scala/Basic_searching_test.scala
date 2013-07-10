ed2b277fc3038dd317751b2f30e16aaf853b614e
nick <nick@ntcoding.co.uk>
2013-07-10 20:09:47 +0100

    added simple query example

---
+++ lucene_in_action/chapter3/src/test/scala/Basic_searching_test.scala
@@ -0,0 +1,31 @@
+import java.io.File
+import org.apache.lucene.index.Term
+import org.apache.lucene.search.{TermQuery, Query, IndexSearcher}
+import org.apache.lucene.store.FSDirectory
+import org.scalatest.FreeSpec
+import org.scalatest.MustMatchers
+
+class Basic_searching_test extends FreeSpec with MustMatchers {
+
+    "After opening a searcher on an index that contains content with ant and junit" - {
+        val dir = FSDirectory.open(new File("/mnt/repos/lia2e/build/index"))
+        val searcher = new IndexSearcher(dir)
+
+        "A search for ant finds the item containing Ant" in {
+            val antQuery = new TermQuery(new Term("subject", "ant"))
+            val docs = searcher.search(antQuery, 10)
+            docs.totalHits must equal(1)
+        }
+
+        "A search for junit finds the items containing Junit" in {
+            val junitQuery = new TermQuery(new Term("subject", "junit"))
+            val docs = searcher.search(junitQuery, 10)
+            docs.totalHits must equal(2)
+        }
+
+        "_ Cleanup" in {
+            searcher close()
+            dir close()
+        }
+    }
+}
