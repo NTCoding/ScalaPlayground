553c112507396bef69d59555866dc5bbbac71386
nick <nick@ntcoding.co.uk>
2013-10-20 16:49:35 +0100

    more implicit conversion examples

--- scala_in_depth/src/main/scala/Implicits/Implicit_conversion_examples.scala
+++ scala_in_depth/src/main/scala/Implicits/Implicit_conversion_examples.scala
@@ -1,12 +1,13 @@
 package Implicits
 
 class ImplicitConversions {
-  def implicitConversionCanComeFromLocalScope(implicit g: Gorilla) = println(g.name)
-  def implicitConversionsCanComeFromImportedScope(implicit p: Panda) = println(p.name)
-  def implicitConversionsCanComeFromCompanion(implicit e: Elephant) = println(e.name)
-  def implicitConversionsCanComeFromBaseCompanion(implicit j: Joker) = println(j.name)
-  def implicitConversionsCanComeFromGenericType(implicit t: List[Turtle]) = println(t map (_.name) mkString ",")
-  def implicitConversionsCanComeFromPackageObject(implicit m: Moose) = println(m.name)
+  def implicitConversionCanComeFromLocalScope(g: Gorilla) = println(g.name)
+  def implicitConversionsCanComeFromImportedScope(p: Panda) = println(p.name)
+  def implicitConversionsCanComeFromCompanion(e: Elephant) = println(e.name)
+  def implicitConversionsCanComeFromBaseCompanion(j: Joker) = println(j.name)
+  def implicitConversionsCanComeFromGenericType(t: List[Turtle]) = println(t map (_.name) mkString ",")
+  def implicitConversionsCanComeFromPackageObject(m: Moose) = println(m.name)
+  def implicitConversionAreFoundInFromType(m: Moose) = println(m.name)
 }
 
 case class Gorilla(name: String)
@@ -33,6 +34,17 @@ object Turtle {
   implicit def stringToTurtle(s: String): Turtle = new Turtle(s)
 }
 
+object Panda {
+  implicit def pandaToMoose(p: Panda): Moose = new Moose(p.name)
+}
+
+object Moose {
+  // uncomment the below will make compiler blow - it exists in panda above too
+  //implicit def pandaToMoose(p: Panda): Moose = new Moose("Should not get to this code")
+}
+
+class DooHickey { def wow() { println("wowzers")} }
+
 object Implicit_conversion_examples {
   def main(args: Array[String]) {
     val imp = new ImplicitConversions
@@ -51,5 +63,14 @@ object Implicit_conversion_examples {
     imp.implicitConversionsCanComeFromGenericType(List("little tuts", "big tuts"))
 
     imp.implicitConversionsCanComeFromPackageObject("I wanna be a moose")
+
+    val valsCanBeConvertedToo: Panda = "I'm a string not a panda"
+
+    implicit def stringToDooHickey(s: String): DooHickey = new DooHickey
+    "I'm not a DooHickey".wow() // implicit conversion - wow does not exist on string
+
+    // The conversion can live in panda or moose - but not both
+    imp.implicitConversionAreFoundInFromType(new Panda("I'm a Panda not a Moose"))
+
   }
 }
