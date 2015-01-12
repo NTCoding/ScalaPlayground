1597e677e21402ee2f5d3843a540447f98b3deaf
nick <nick.tune@artirix.com>
2015-01-12 22:14:59 +0000

    updated spark user journeys example to make it easier to follow on blog

--- spark/common_user_journeys/src/main/scala/Demo.scala
+++ spark/common_user_journeys/src/main/scala/Demo.scala
@@ -9,12 +9,14 @@ import org.apache.spark.SparkContext._
 object Demo {
   Logger.getRootLogger.setLevel(Level.ERROR)
 
-  def main(args: Array[String]): Unit = {
-    // this could also be an Amazon s3 file path. e.g: s3n://username:password@host/bucket/logfile.log.gz
+  def main(args: Array[String]) {
+    val spark = createEmbeddedSparkInstance()
+    
+    // could also be an Amazon s3 file path. e.g: s3n://username:password@host/bucket/logfile.log.gz
     val testDataFilePath = this.getClass.getResource("sample_events.log").getPath
 
-    val spark = createInMemorySparkInstance()
-    val logs = spark.textFile(testDataFilePath)
+    // reads each line from the log file as a string
+    val logs: RDD[String] = spark.textFile(testDataFilePath) 
 
     // reduce the working set first to improve performance
     // RDD is the Spark abstraction
@@ -27,29 +29,35 @@ object Demo {
 
     val sortedSessions = sortEachSessionChronologically(userSessions).filter(_._2.length > 0)
 
-    val journeys: RDD[(String, Int)] = sortedSessions.map(s => (s._2.map(_.url.get).mkString(","), 1))
+    val journeys: RDD[(String, Int)] = sortedSessions.map {
+      s => (s._2.map(_.url.get).mkString(","), 1)
+    }
 
     // use second item in journey tuples to count occurences of that journey
     val commonJourneysWithTotals = journeys.reduceByKey((count1, count2) => count1 + count2)
                                            .sortBy(-_._2) // sort by count descending
 
+    printToConsole(commonJourneysWithTotals.take(10))
+    
+    spark.stop()
+  }
+
+  private def printToConsole(journeys: Seq[(String, Int)]) {
     // print the results to the console
-    commonJourneysWithTotals.take(10).foreach { j =>
+    journeys.foreach { j =>
       val journey = j._1.replace("http://", "") // easier to discern in console
       val count = j._2
       println(s"$count - $journey")
     }
-
-    spark.stop()
   }
 
-  private def createInMemorySparkInstance() = {
-    // see spark documentation for explanation of thse: https://spark.apache.org/docs/1.1.1/configuration.html
+  private def createEmbeddedSparkInstance() = {
+    // see spark documentation: https://spark.apache.org/docs/1.1.1/configuration.html
     val conf = new SparkConf()
       .set("spark.storage.memoryFraction", "0.1")
       .set("spark.executor.memory", "3g")
 
-    new SparkContext(master = "local", appName = "common_user_journey_example", conf)
+    new SparkContext(master = "local", appName = "common_user_journeys_example", conf)
   }
 
   private def sortEachSessionChronologically(userSessions: RDD[(Option[String], Iterable[Event])]) = {
@@ -58,12 +66,6 @@ object Demo {
       val pageViews = session._2
       val chronologicalPageViews = pageViews.toSeq.sortBy(pv => DateTime.parse(pv.timestamp.get))(orderer)
       (sessionId, chronologicalPageViews.take(3)) // just using first 3 pageview for this example
-    }
-  }
-
-  private def parseJourneys(sessions: RDD[(Option[String], Seq[Event])]) = {
-    sessions.map { session =>      val journey = session
-      (session._1, journey)
     }
   }
 
