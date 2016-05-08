ae1b550d14ab77a9a81549aa2c1d2430c4a507ef
nick <nick@ntcoding.co.uk>
2016-05-08 13:41:10 +0100

    add error checking to djikstra

--- algos/algos/src/test/scala/DjikstraSpec.scala
+++ algos/algos/src/test/scala/DjikstraSpec.scala
@@ -1,78 +1,91 @@
 import org.scalatest.{FreeSpec, Matchers}
 
 import scala.annotation.tailrec
-import scala.io.Source
 
 class DjikstraSpec extends FreeSpec with Matchers {
-  import Djikstra.Vertex
+  import Djikstra.Edge
 
   "finds shortest path from source to each non-source node and penultimate node on path" - {
 
     "for graph with 3 nodes and only 1 path to each" in {
-      val graph = Array(Vertex(0, 1, 5), Vertex(0, 2, 3))
+      val graph = Array(Edge(0, 1, 5), Edge(0, 2, 3))
       assert(Djikstra(graph, 0).weights === Array(0, 5, 3))
       assert(Djikstra(graph, 0).previousNodes === Array(Djikstra.noNode, 0, 0))
     }
 
-    "for a 3-level deep graph with only 1 path to each node (a tree)" in {
+    "for a 3 level deep graph with only 1 path to each node (a tree)" in {
       val graph = Array(
-        Vertex(0, 1, 5), Vertex(1, 2, 4), Vertex(1, 3, 3), Vertex(0, 4, 6), Vertex(4, 5, 7), Vertex(4, 6, 25)
+        Edge(0, 1, 5), Edge(1, 2, 4), Edge(1, 3, 3), Edge(0, 4, 6), Edge(4, 5, 7), Edge(4, 6, 25)
       )
       assert(Djikstra(graph, 0).weights === Array(0, 5, 9, 8, 6, 13, 31))
       assert(Djikstra(graph, 0).previousNodes === Array(Djikstra.noNode, 0, 1, 1, 0, 4, 4))
     }
 
-    "for a 3-level deep graph with only 1 path to each node when the source is not 0" in {
+    "for a 3 level deep graph with only 1 path to each node when the source is not 0" in {
       val graph = Array(
-        Vertex(0, 4, 6), Vertex(4, 5, 7), Vertex(4, 6, 25), Vertex(1, 0, 5), Vertex(1, 3, 3), Vertex(2, 1, 4)
+        Edge(0, 4, 6), Edge(4, 5, 7), Edge(4, 6, 25), Edge(1, 0, 5), Edge(1, 3, 3), Edge(2, 1, 4)
       )
       assert(Djikstra(graph, 2).weights === Array(9, 4, 0, 7, 15, 22, 40))
       assert(Djikstra(graph, 2).previousNodes === Array(1, 2, Djikstra.noNode, 1, 0, 4, 4))
     }
+
+    "for a 7 node graph with multiple paths to some nodes" in {
+      val graph = Array(
+        Edge(0, 1, 7), Edge(1, 2, 5), Edge(2, 5, 1), Edge(5, 6, 1),
+        Edge(0, 3, 3), Edge(3, 4, 3), Edge(4, 2, 1), Edge(4, 6, 10)
+      )
+      assert(Djikstra(graph, 0).weights === Array(0, 7, 7, 3, 6, 8, 9))
+      assert(Djikstra(graph, 0).previousNodes === Array(Djikstra.noNode, 0, 4, 0, 3, 2, 5))
+    }
+  }
+
+  "mandates that all nodes are sequentially numbered starting from 0" in {
+    intercept[Djikstra.NonSequentialNodeNumbers](Djikstra(Array(Edge(0, 1, 5), Edge(0, 3, 5)), 0))
   }
 }
 
 object Djikstra {
-  case class Vertex(start: Int, end: Int, weight: Int)
+  val infinity = Integer.MAX_VALUE
+  val noNode = -33
+
+  def apply(graph: Array[Edge], source: Int): ShortestPaths = {
+    val nodes = graph.map(_.end).distinct
+    if ((nodes ++ Seq(source)).sorted.toSeq != (0 to nodes.max)) throw NonSequentialNodeNumbers()
+    findShortestPaths(graph :+ Edge(source, source, 0), (0 to nodes.length).toArray, initialiseShortestPaths(nodes.length + 1, source))
+  }
+
+  private def initialiseShortestPaths(numberOfNodes: Int, source: Int) =
+    ShortestPaths(
+      Array.fill(numberOfNodes)(Djikstra.infinity).updated(source, 0),
+      Array.fill(numberOfNodes)(Djikstra.noNode)
+    )
+
+  @tailrec
+  private def findShortestPaths(graph: Seq[Edge], unchecked: Array[Int], shortestPaths: ShortestPaths): ShortestPaths =
+    if (unchecked.isEmpty) shortestPaths else {
+      val shortestPathVertex = unchecked.map(u => (u, shortestPaths.weights(u))).sortBy(_._2).head._1
+      val adjacent = graph.filter(_.start == shortestPathVertex)
+      val sps = relax(shortestPathVertex, adjacent, shortestPaths)
+      findShortestPaths(graph, unchecked.filterNot(_ == shortestPathVertex), sps)
+    }
+
+  @tailrec
+  private def relax(vertex: Int, adjacent: Seq[Edge], shortestPaths: ShortestPaths): ShortestPaths =
+    if (adjacent.isEmpty) shortestPaths else {
+      val totalPathWeight = shortestPaths.weights(vertex) + adjacent.head.weight
+      if (totalPathWeight < shortestPaths.weights(adjacent.head.end)) {
+        val sps = shortestPaths.update(adjacent.head.end, totalPathWeight, vertex)
+        relax(vertex, adjacent.tail, sps)
+      } else relax(vertex, adjacent.tail, shortestPaths)
+    }
+
+  case class Edge(start: Int, end: Int, weight: Int)
 
   case class ShortestPaths(weights: Array[Int], previousNodes: Array[Int]) {
     def update(node: Int, weight: Int, previousNode: Int) =
       copy(weights = weights.updated(node, weight), previousNodes.updated(node, previousNode))
   }
 
-  val infinity = Integer.MAX_VALUE
-  val noNode = -33
-
-  def apply(graph: Array[Vertex], source: Int): ShortestPaths =
-    findShortestPaths(graph :+ Vertex(source, source, 0), initialiseShortestPaths(graph, source))
-
-  private def initialiseShortestPaths(graph: Array[Vertex], source: Int) =
-    ShortestPaths(
-      Array.fill(graph.length + 1)(Djikstra.infinity).updated(source, 0),
-      Array.fill(graph.length + 1)(Djikstra.noNode)
-    )
-
-  @tailrec
-  private def findShortestPaths(unchecked: Array[Vertex], shortestPaths: ShortestPaths): ShortestPaths =
-    unchecked.isEmpty match {
-      case true => shortestPaths
-      case false =>
-        val shortestPathVertex = unchecked.map(v => (v, shortestPaths.weights(v.end))).sortBy(_._2).head._1
-        val adjacent = unchecked.filter(_.start == shortestPathVertex.end)
-        val sps = relax(shortestPathVertex, adjacent, shortestPaths)
-        findShortestPaths(unchecked.filterNot(_ == shortestPathVertex), sps)
-    }
-
-  @tailrec
-  private def relax(vertex: Vertex, adjacentVertices: Seq[Vertex], shortestPaths: ShortestPaths): ShortestPaths =
-    adjacentVertices.isEmpty match {
-      case true => shortestPaths
-      case false =>
-        val totalPathWeight = shortestPaths.weights(vertex.end) + adjacentVertices.head.weight
-        if (totalPathWeight < shortestPaths.weights(adjacentVertices.head.end)) {
-          val sps = shortestPaths.update(adjacentVertices.head.end, totalPathWeight, vertex.end)
-          relax(vertex, adjacentVertices.tail, sps)
-        } else relax(vertex, adjacentVertices.tail, shortestPaths)
-    }
+  case class NonSequentialNodeNumbers(msg: String = "") extends Exception(msg)
 }
 
